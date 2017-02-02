4214528a7b77c1459c37f5aafa2b6280cbdff591
Shailesh Kumar <012skp@gmail.com>
2017-02-02 09:51:29 +0530

    lskdf

---
+++ shell/shell.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<unistd.h>
+
+using namespace std;
+
+
+int main(){
+	char buff[100];
+	int fd;
+	//fd = open("console",O_RDWR);
+	//if(fd >= 3){ close(fd); exit(1);}
+	
+	char cmd[100];
+	scanf("%s",cmd);
+	printf("%s",cmd);
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
