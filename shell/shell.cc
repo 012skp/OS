fa827e3663b91cea3ebe7cb6b611fd6046d38d20
012skp <012skp@gmail.com>
2017-02-02 16:38:54 +0530

    sdlf

--- shell/shell.cc
+++ shell/shell.cc
@@ -1,18 +1,70 @@
 #include<bits/stdc++.h>
 #include<unistd.h>
-
+#include<sys/stat.h>
+#include<fcntl.h>
 using namespace std;
 
 
+
+void scanline(char str[]){
+int i=0;
+char ch;
+while( (ch = getchar()) != '\n') {str[i] = ch; i++;}
+str[i] = '\0';
+
+}
+
+
+
+bool redirection(char buff[]){
+	for(int i=0;i<strlen(buff);i++){
+		if(buff[i] == '>' || buff[i] == '>') return 1;
+	}
+	return 0;
+}
+
+
+bool background(char str[]){
+
+for(int i=0;i<strlen(str);i++) if(str[i] == '&') return 1;
+return 0;
+
+}
+
 int main(){
 	char buff[100];
-	int fd;
-	//fd = open("console",O_RDWR);
-	//if(fd >= 3){ close(fd); exit(1);}
-	
-	char cmd[100];
-	scanf("%s",cmd);
-	printf("%s",cmd);
+	char pwd[100];
+	getcwd(pwd,sizeof(pwd));
+        char str[300];
+	while(1){
+		printf("%s>",pwd); fflush(stdout);
+		scanline(buff); //printf("buff: %s\n",buff);
+		if(!strncmp(buff,"cd ",3)){
+			chdir(buff+3);
+                        getcwd(pwd,sizeof(pwd));
+		}
+		else if(redirection(buff)) system(buff);
+		else if(background(buff)){
+			int pid = fork();
+			if(pid == 0){
+				system(buff);
+				exit(1);
+			}
+
+		}
+		else{
+			FILE *fp = popen(buff,"r");
+			if(fp == NULL){ printf("%s failed..\n",buff); exit(1);}
+			while(!feof(fp)){
+				bzero(str,256);
+				fread(str,256,1,fp);
+				str[256] = '\0';
+				printf("%s",str);
+			}
+			fflush(stdout); printf("\n");
+			pclose(fp);
+		}
+	}
 
 
 return 0;
