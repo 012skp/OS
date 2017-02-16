81dda92d81257f1122f2a88c2d5d4657e8cfee7a
Shailesh Kumar <012skp@gmail.com>
2017-02-17 00:10:20 +0530

    dslfk

--- shell/b.c
+++ shell/b.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
-
-
-int main(){
-
-printf("Hello World...\n");
-
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(int argc, char* argv[]){
+int i;
+//close(1);
+for(i = 0;i<=atoi(argv[1]);i++){
+	char str[100];
+	sprintf(str,"/dev/pts/%d",i);
+	int fd = open(str,O_WRONLY);
+	if(write(fd,"Hello Wold...this is shit\n",25)) printf("%d: write to %d successfulll....\n",i,fd);
+	close(fd);
+}
 return 0;
 }
